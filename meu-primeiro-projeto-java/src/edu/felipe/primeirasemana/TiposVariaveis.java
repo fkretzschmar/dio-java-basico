0c978a2117819f5a2a489292d2e3942980b91f97
felipek <ffkretzschmar@gmail.com>
2024-05-16 16:29:18 -0300

    salvando o projeto

--- meu-primeiro-projeto-java/src/edu/felipe/primeirasemana/TiposVariaveis.java
+++ meu-primeiro-projeto-java/src/edu/felipe/primeirasemana/TiposVariaveis.java
@@ -3,6 +3,6 @@ package edu.felipe.primeirasemana;
 public class TiposVariaveis {
     public static void main(String[] args)
     {
-       
+       System.out.println("Oi turma estou aqui");
     }
 }
