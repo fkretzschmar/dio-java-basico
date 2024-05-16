8a62cac934312a8219b6b6a8df3adb3778309dd3
felipek <ffkretzschmar@gmail.com>
2024-05-16 00:44:46 -0300

    Smart tv

--- meu-primeiro-projeto-java/src/edu/felipe/primeirasemana/TiposVariaveis.java
+++ meu-primeiro-projeto-java/src/edu/felipe/primeirasemana/TiposVariaveis.java
@@ -3,14 +3,6 @@ package edu.felipe.primeirasemana;
 public class TiposVariaveis {
     public static void main(String[] args)
     {
-       String primeiroNome = "Felipe";
-       String SegundoNome = "Kretzschmar";
-
-       String nomeCompleto = nomeCompleto(primeiroNome, SegundoNome);
-
-        System.out.println(nomeCompleto);
-    }
-    public static String nomeCompleto (String primeiroNome, String segundoNome){
-        return primeiroNome + (" ") + (segundoNome);
+       
     }
 }
