264836208f5aad5dc243243d3527b4ba1bff6064
felipek <ffkretzschmar@gmail.com>
2024-05-17 13:38:04 -0300

    operadora

---
+++ switch-plano-operadora/src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       
+        String plano = "T";
+
+        switch (plano) {
+            case "T":
+                System.out.println("5Gb Youtube");
+                
+            case "M":
+                System.out.println("Whats e Instagram grátis");
+               
+            case "B":
+                System.out.println("100 minutos de ligação");
+                
+            default:
+                System.out.println("Plano não encontrado");
+                break;
+        }
+    }
+}
