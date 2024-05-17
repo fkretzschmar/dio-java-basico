eb9d3896aed6482218839e2466bda781bcc890a9
felipek <ffkretzschmar@gmail.com>
2024-05-17 17:25:44 -0300

    Array

---
+++ ExemploArray/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String alunos [] = {"Felipe","Jonas","Julia","Marcos",};
+
+        for(int x=0;x<alunos.length;x++) {
+
+        System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+
+
+    }
+}
+}
